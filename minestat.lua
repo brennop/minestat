a4caeec9a806db7d117116909bb4bccb2ecad63c
brennop <brennopcordeiro@gmail.com>
2022-12-17 12:57:09 -0300

    use http.lua library

--- minestat.lua
+++ minestat.lua
@@ -1,6 +1,7 @@
 #!/usr/bin/env lua
 
 local socket = require "socket"
+local http = require "http.server"
 local json = require "json"
 
 local bit = bit32 or require "bit"
@@ -44,7 +45,6 @@ local function ping()
   sock:send(table.concat(handshake))
   sock:send(string.char(0x01, 0x00)) -- status request
 
-  -- this pcall is ugly, wish I could check if the request failed some other way
   local ok, size = pcall(read_varint, sock)
   if not ok then return end
 
@@ -55,67 +55,28 @@ local function ping()
   return json.decode(data)
 end
 
-local function handle_client(client)
-  local method, uri, version = client:receive():match("(%u+)%s([%p%w]+)%s(HTTP/1.1)")
+http.get("/", function()
+  local status = ping()
 
-  -- ignore headers
-  for line in function() client:receive() end do end
+  if status then
+    local players = {}
 
-  if method == "GET" and uri == "/" then
-    local status = ping()
-    local body = ""
-
-    if status then
-      local players = {}
-
-      if status.players.sample then
-        for _, player in ipairs(status.players.sample) do
-          table.insert(players, "  - " .. player.name)
-        end
+    if status.players.sample then
+      for _, player in ipairs(status.players.sample) do
+        table.insert(players, "  - " .. player.name)
       end
-
-      body = format:format(
-        status.version.name,
-        status.description.text,
-        status.players.online,
-        status.players.max,
-        table.concat(players, "\n")
-      )
-    else
-      body = "Server is offline!\n"
     end
 
-    local response = {
-      "HTTP/1.1 200 OK",
-      "Content-Type: text/plain",
-      "Content-Length: " .. #body,
-      "",
-      body,
-      "",
-    }
-
-    client:send(table.concat(response, "\r\n"))
+    return format:format(
+      status.version.name,
+      status.description.text,
+      status.players.online,
+      status.players.max,
+      table.concat(players, "\n")
+    )
   else
-    client:send("HTTP/1.1 404 Not Found\r\n\r\n")
+    return "Server is offline!"
   end
-end
+end)
 
-local function handle_coroutine(client)
-  client:settimeout(1)
-  local ok, err = pcall(handle_client, client)
-  if not ok then 
-    client:send("HTTP/1.1 500 Internal Server Error\r\n\r\n")
-  end
-  client:close()
-end
-
-local server = socket.bind("*", 3000)
-print("Listening on port 3000")
-
-while true do
-  local client = server:accept()
-  if client then
-    local co = coroutine.create(handle_coroutine)
-    coroutine.resume(co, client)
-  end
-end
+http.listen(3000)
